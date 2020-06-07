ceb7d026785d612ba96841a29cdaf58292eeaaea
Frank Fang <frankfang2014@hotmail.com>
2020-06-07 15:25:01 -0700

    making request for swiperList data

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,16 +1,18 @@
 //Page Object
 Page({
-  data: {},
+  data: {
+    swipeList: [],
+  },
   //options(Object)
-  onLoad: function (options) {},
-  onReady: function () {},
-  onShow: function () {},
-  onHide: function () {},
-  onUnload: function () {},
-  onPullDownRefresh: function () {},
-  onReachBottom: function () {},
-  onShareAppMessage: function () {},
-  onPageScroll: function () {},
-  //item(index,pagePath,text)
-  onTabItemTap: function (item) {},
+  onLoad(option) {
+    var reqTask = wx.request({
+      url: "https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata",
+      success: (result) => {
+        console.log(result);
+        this.setData({
+          swipeList: result.data.message,
+        });
+      },
+    });
+  },
 });
