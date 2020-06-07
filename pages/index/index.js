1e22dfc3b8b5da1431a5892156aff7626382139c
Frank Fang <frankfang2014@hotmail.com>
2020-06-07 16:05:43 -0700

    adding categories navigators

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,14 +3,30 @@ import { request } from "../../request/index";
 Page({
   data: {
     swipeList: [],
+    categories: [],
   },
   //options(Object)
   onLoad(option) {
+    //request swiper
+    this.getSwiperList();
+    // request category
+    this.getCategories();
+  },
+  getSwiperList() {
     request({
       url: "https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata",
     }).then((res) => {
       this.setData({
         swipeList: res.data.message,
+      });
+    });
+  },
+  getCategories() {
+    request({
+      url: "https://api-hmugo-web.itheima.net/api/public/v1/home/catitems",
+    }).then((res) => {
+      this.setData({
+        categories: res.data.message,
       });
     });
   },
