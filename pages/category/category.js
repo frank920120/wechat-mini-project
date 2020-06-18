05e4e815b9d0a6eb9abedf20232a3befacdde0cb
Frank Fang <frankfang2014@hotmail.com>
2020-06-18 16:41:46 -0700

    get categories data

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,66 +1,32 @@
 // pages/category/category.js
+import { request } from "../../request/index";
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    categories: [],
+    leftMenuList: [],
+    rightContent: [],
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    this.getCate();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  getCate() {
+    request({
+      url: "https://api-hmugo-web.itheima.net/api/public/v1/categories",
+    }).then((res) => {
+      this.setData(
+        {
+          categories: res.data.message,
+        },
+        () => {
+          let leftMenuList = this.data.categories.map((v) => v.cat_name);
+          let rightContent = this.data.categories[0].children;
+          this.setData({
+            leftMenuList,
+            rightContent,
+          });
+        }
+      );
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+});
